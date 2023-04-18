36f38302d4807d659e3e7a95581f1e60026831ed
jorge <jorgefreire2403@gmail.com>
2023-04-18 17:09:11 -0400

    cambio

--- ProyectoCondominio/core/urls.py
+++ ProyectoCondominio/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from . import views 
 from core.views import login,nosotros
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
@@ -8,5 +8,5 @@ urlpatterns = [
     
     path('',views.index,name='index'),
     path('login',views.login,name='login'),
-    path('nosotros',views.login,name='nosotros'),
+    path('nosotros',views.nosotros,name='nosotros'),
     ]
